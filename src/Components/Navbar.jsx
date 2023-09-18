e806f6cc50b750f75ce4cc207f5e8acff9d08b85
AntonyJauregui <139800467+AntonyJauregui@users.noreply.github.com>
2023-09-18 18:23:58 -0500

    2023.09.18

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ export const Navbar = () => {
     return (
         <nav>
             <div>
-                <h1>Tarea 1 Pregunta 2</h1>
+                <h2>Primera tarea</h2>
             </div>
         </nav>
     )}
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,24 +1,61 @@
 import { Navbar } from "./Components/Navbar"
 import { Tareas } from "./Components/Tareas";
+
 export const TaskList = () =>{
+
     const TareasRutinarias = [ 
-        {id: 1, Texto: "Tender la cama", Estado: "Realizado",},
-        {id: 2, Texto: "Barrer el cuarto", Estado: "Realizado",},
-        {id: 3, Texto: "Bañarse", Estado: "Realizado",},
-        {id: 4, Texto: "Preparar el desayuno", Estado: "Realizado",},
-        {id: 5, Texto: "Alistar las cosas", Estado: "Realizado",},
-        {id: 6, Texto:"Desayunar", Estado: "Realizado",},
-        {id: 7, Texto:"Ir al trabajo", Estado: "Pendiente",className:"letra"}];
+        { 
+            id: 1, 
+            Texto: "Tender la cama", 
+            Estado: "Realizado",
+        },
+
+        {
+            id: 2, 
+            Texto: "Barrer el cuarto",
+             Estado: "Realizado",
+         },
+        { 
+            id: 3, 
+            Texto: "Bañarse", 
+            Estado: "Realizado",
+        },
+        {
+            id: 4, 
+            Texto: "Preparar el desayuno", 
+            Estado: "Realizado",
+        },
+        {
+            id: 5, 
+            Texto: "Alistar las cosas", 
+            Estado: "Realizado",
+        },
+        {
+            id: 6, 
+            Texto:"Desayunar", 
+            Estado: "Realizado",
+        },
+        {
+            id: 7, 
+            Texto:"Ir al trabajo", 
+            Estado: "Pendiente",
+        }
+    ];
+
+
 
     return(
         <section className="container">
             <Navbar />
+            <div className="containertarea">
             {
             TareasRutinarias.map((Tarea)=>(
 
                 < Tareas {...Tarea}/>
             ))
             }
+        </div>
         </section>
+        
     )
 }
